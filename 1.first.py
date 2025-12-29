6c6b8a84db0fe48942ecfe691075904a8b7e75b0
sarth4161 <sarthakpatil99104@gmail.com>
2025-12-29 19:56:09 +0530

    1st commit

---
+++ 1.first.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+JOBS=[
+    {'id':1,
+    'title':'Data Analyst',
+    'location':'Bengaluru, India',
+    'salary': 'Rs. 15,00,000'},
+    {'id':2,
+    'title':'Data Scientist',
+    'location':'Delhi, India',
+    'salary': 'Rs. 15,00,000'},
+    {'id':3,
+    'title':'Frontend Engineer',
+    'location':'Remot'},
+    {'id':4,
+    'title':'Backend Engineer',
+    'location':'San Francisco, USA',
+    'salary': '$ 120,000'}
+
+]
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("home.html",jobs=JOBS)
+
+if __name__ == "__main__":
+    app.run(debug=True)
